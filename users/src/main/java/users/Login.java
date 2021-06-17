678700029c598d27eb76de0aa365daa495d590ca
Jay <50858217+jaysampath@users.noreply.github.com>
2021-06-17 22:05:51 +0530

    Update Login.java

--- users/src/main/java/users/Login.java
+++ users/src/main/java/users/Login.java
@@ -12,9 +12,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import java.sql.*;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
+	
+	String url="",uname1="",pass1="",query="";
+	Connection con;
+	PreparedStatement ps;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		
@@ -24,27 +30,51 @@ public class Login extends HttpServlet {
 		ArrayList<String> un1 = (ArrayList<String>)session.getAttribute("unames");
 		ArrayList<String> pwd1 = (ArrayList<String>)session.getAttribute("passwords");
 		PrintWriter out = response.getWriter();
-		if(!un1.contains(uname)) {
+		ArrayList<String> check_unames = new ArrayList<>();
+		ArrayList<String> check_pw = new ArrayList<>();
+		
+		try {
+			 url = "jdbc:mysql://localhost:3306/db1";
+			 uname1 = "root";
+			 pass1 = "Jayasampath@200";
+			 
+			Class.forName("com.mysql.jdbc.Driver");
+			 con = DriverManager.getConnection(url,uname1,pass1);
+			 query = "select * from task3_users";
+				Statement stmt=con.createStatement(); 
+				 ResultSet rs=stmt.executeQuery(query);
+				 System.out.println("Table after new User: ");
+				 while(rs.next()) { 
+				 System.out.println(rs.getString(1)+"  | |   "+rs.getString(2)); 
+				 check_unames.add(rs.getString(1));
+				 check_pw.add(rs.getString(2));
+				 }
+			}
+			catch(Exception e) {
+				System.out.println("Exception while connecting: "+e);
+			}
+		
+		if(!check_unames.contains(uname)) {
 			out.println("<p>Not a registered User </p>");
 			out.println("<a href=\"signup.jsp\">Signup here!</a>");
 		}
 		else {
 	    int x=-1;
 	    int y=-1;
-		for(int i=0;i<un1.size();i++) {
-			System.out.println("Iteration: "+un1.get(i)+" "+pwd1.get(i));
-	    	if(un1.get(i).equals(uname)&&pwd1.get(i).equals(pass)){
+		for(int i=0;i<check_unames.size();i++) {
+			//System.out.println("Iteration: "+check_unames.get(i)+" "+check_pw.get(i));
+	    	if(check_unames.get(i).equals(uname)&&check_pw.get(i).equals(pass)){
 	    		
-	    		 x = un1.indexOf(uname); 
-	    		 y = pwd1.indexOf(pass);
+	    		 x = check_unames.indexOf(uname); 
+	    		 y = check_pw.indexOf(pass);
                 
 	    		 System.out.println("index: "+x+" "+y);
 	    	
 	    	}
 	    }
 		
-		System.out.println("Usernames List:  "+un1);
-		System.out.println("Passwords Lits:   "+pwd1);
+		//System.out.println("Usernames List:  "+check_unames);
+		//System.out.println("Passwords Lits:   "+check_pw);
 		 if(  x!=-1 && x==y){
 	    		response.sendRedirect("welcome.jsp");
  	   }else {
